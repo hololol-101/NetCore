9f20121dcb3af3c89b42b62bf417238dbfc6f4c1
kangeun.noh@lotte.net <kangeun.noh@lotte.net>
2022-05-11 09:33:03 +0900

    Add Login funcion using MVC pattern

---
+++ NetCore.Web/Models/LoginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.Web.Models
+{
+    public class LoginInfo
+    {
+        [Required(ErrorMessage ="사용자 아이디를 입력하세요.")]
+        [MinLength(6, ErrorMessage ="사용자 아이디를 6자 이상 입력하세요.")]
+        [Display(Name="사용자 아이디")]
+        public string UserId { get; set; }
+        [Required(ErrorMessage = "비밀번호를 입력하세요.")]
+        [MinLength(6, ErrorMessage = "비밀번호를 6자 이상 입력하세요.")]
+        [Display(Name ="비밀번호")]
+        public string Password { get; set; }
+    }
+}
